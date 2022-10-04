9619763b9bbf2b7b4c3888c4d6087ffcb500538c
Rhegis Marques <rhegismarques@gmail.com>
2022-10-04 17:55:08 -0300

    first commit

--- Lanchonete.c
+++ Lanchonete.c
@@ -1,6 +1,9 @@
 	#include <stdio.h>
 	#include <stdlib.h>
+	#include <string.h>
 	#include <time.h>
+	#define MAX_PRODUTOS 10
+	#define MAX_COMANDAS 10
 	
 	int op;
 	
@@ -11,13 +14,16 @@
 	  int tempoDeInicio;
 	  int tempoDeFim;   
 	}Comanda;
-	
+	Comanda comandas[MAX_COMANDAS];
+
 	/* Estrutura Produto*/
 	typedef struct{  
 	  int codigo;
+		int ativo;
 	  char nome[10];
-	  float valor;
+	  float valor[4];
 	}Produto;
+	Produto produtos[MAX_PRODUTOS];
 	 
 	void menuProduto();
 	void menuComanda(); 
@@ -29,9 +35,10 @@
 	}
 	
 	void menu(){
-	  system("cls"); 
+	   
 	  int op;
 	    do{
+				system("cls");
 	
 	      printf("|==============================|\n");
 	      printf("|     LANCHONETE RUN           |\n");
@@ -41,7 +48,7 @@
 	      printf("|==============================|\n");
 	      printf("Seleciona uma Opcao: ");  
 	      scanf("%d", &op);
-	
+				getchar();
 	      switch(op){
 	        case 1:
 	        menuComanda();
@@ -58,7 +65,10 @@
 	      
 	    }while (op!=0);
 	}
-	
+	void cadastrarComanda();
+	void excluirComanda();
+	void listarComanda();
+
 	void menuComanda(){
 	   do{
 	    system("cls");
@@ -66,7 +76,7 @@
 	      printf("#==============================#\n");
 	      printf("|           COMANDA            |\n");
 	      printf("|------------------------------|\n");	
-	      printf("\n\t1 - CADASTRAR\n\t2 - EXCLUIR\n\t3 - PESQUISAR\n\t0 - SAIR\n\n");
+	      printf("\n\t1 - CADASTRAR\n\t2 - EXCLUIR\n\t3 - LISTAR\n\t0 - SAIR\n\n");
 	      printf("|------------------------------|\n");
 	      printf("|==============================|\n");
 	      printf("Seleciona uma Opcao: ");  
@@ -74,15 +84,15 @@
 
         switch(op){
 	        case 1:
-	        Cadastrar();
+	        cadastrarComanda();
 	        break;
 	        
 	        case 2:
-	        excluir();				
+	        excluirComanda();				
 	        break;
 
           case 3:
-	        pesquisar();				
+	        listarComanda();				
 	        break;
 	        
 	        case 0:				
@@ -94,31 +104,37 @@
 	    }while (op!= 0);
 	
 	}
-	
+
+	void cadastrarProduto();
+	void excluirProduto();
+	void listarProduto();
+
 	void menuProduto(){
-	  system("cls");
+	  
 	
 	  do{
+			system("cls");
 	
 	      printf("|==============================|\n");
 	      printf("|           PRODUTO            |\n");
 	      printf("|------------------------------|\n");	
-	      printf("\n\t1 - CADASTRAR\n\t2 - EXCLUIR\n\t3 - PESQUISAR\n\t0 - SAIR\n\n");
+	      printf("\n\t1 - CADASTRAR\n\t2 - EXCLUIR\n\t3 - LISTAR\n\t0 - SAIR\n\n");
 	      printf("|------------------------------|\n");
 	      printf("|==============================|\n");
 	      printf("Seleciona uma Opcao: ");  
 	      scanf("%d", &op);
+				getchar();
 	       switch(op){
 	        case 1:
-	        Cadastrar();
+	        cadastrarProduto();
 	        break;
 	        
 	        case 2:
-	        excluir();				
+	        excluirProduto();				
 	        break;
 
           case 3:
-	        pesquisar();				
+	        listarProduto();				
 	        break;
 	        
 	        case 0:				
@@ -130,5 +146,51 @@
 	    }while (op!= 0);
 	
 	}
-	
+	void cadastrarProduto(){
+		int codigo;
+	  char nome[10];
+	  float valor[4];
+		int ativo;
+		int op;
+		do
+		{
+			printf("\nCodigo: ");
+			scanf("%d", &codigo);
+			printf("\nNome: ");
+			fgets(nome, sizeof(nome),stdin);
+			printf("\nValor: ");
+			scanf("%f", &valor[0]);
+			for (int i = 0; i < MAX_PRODUTOS; i++)
+			{
+				if (produtos[i].ativo ==0)
+				strcpy(produtos[i].nome, nome);
+				produtos[i].ativo=1;
+				break;
+			}
+			
+			printf("\n1 - Continuar\n0 - Sair\n");
+			scanf("%d", &op);
+			
+		} while (op!=0);
+		
+	}
+	void excluirProduto(){
+
+	}
+	void listarProduto(){
+		printf("\n LISTA DE PRODUTOS\n");
+		for (int i = 0; i < MAX_PRODUTOS; i++)
+		{
+			if (produtos[i].ativo==1)
+			{
+				
+				printf("Nome: %s\n", produtos[i].nome);
+				printf("\n------------------\n");
+
+			}
+			
+			
+		}
+		
+	}
 	
